f004b96be6a1e0b2c9e23c3c93616742518388b9
Marcos Villanueva <marcosvillanueva009@gmail.com>
2018-08-23 00:35:32 -0300

    Commit: Base de datos Arreglado

--- app/src/main/java/aplicacion/android/stevemorbo/previasfinal/MainActivity.java
+++ app/src/main/java/aplicacion/android/stevemorbo/previasfinal/MainActivity.java
@@ -1,6 +1,7 @@
 package aplicacion.android.stevemorbo.previasfinal;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
@@ -13,18 +14,13 @@ import android.widget.Toast;
 
 import com.google.firebase.analytics.FirebaseAnalytics;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button inicio;
     private TextView registrarse;
-    private EditText nombre;
-    private EditText apellido;
     private EditText mail;
     private EditText contrasena;
-    private Button registro;
     private FirebaseAnalytics mFirebaseAnalytics;
     private FirebaseAuth mAuth;
 
@@ -34,32 +30,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         final Context context = this;
-        Toasts.toastmain(context);
 
         mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
         mAuth = FirebaseAuth.getInstance();
 
-        // Write a message to the database
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("message");
 
-        myRef.setValue("Hello, World!");
 
-        bindUI();
 
         inicio = (Button) findViewById(R.id.iniciarsesion);
         inicio.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                mail = findViewById(R.id.mail);
+                contrasena = findViewById(R.id.contrasena);
                 String mail1 = mail.getText().toString();
                 String contrasena1 = contrasena.getText().toString();
-                boolean mensaje = ServicePersona.iniciarsesion(mail1, contrasena1);
-                if (mensaje){
+                if (ServicePersona.iniciarsesion(mail1, contrasena1)){
                     setContentView(R.layout.pantinicio);
                     Toast.makeText(context, mail1 + " " + contrasena1, Toast.LENGTH_LONG).show();
                     ServicePrevia.cargarprevia();
                 }else {
                     Toast.makeText(context, "Datos incorrectos", Toast.LENGTH_LONG).show();
+
+
                 }
             }
         });
@@ -69,35 +62,25 @@ public class MainActivity extends AppCompatActivity {
         registrarse.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                setContentView(R.layout.pantregistro);
-                registro = (Button) findViewById(R.id.registrarse);
-                registro.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        String nombre1 = nombre.getText().toString();
-                        String apellido1 = apellido.getText().toString();
-                        String mail1 = mail.getText().toString();
-                        String contrasena1 = contrasena.getText().toString();
-                        Persona persona1 = new Persona(nombre1, apellido1, mail1, contrasena1, "", "", "");
-                        Toast.makeText(context, "Nombre " + persona1.nombre, Toast.LENGTH_SHORT).show();
-                        Toast.makeText(context, "Apellido " + persona1.apellido, Toast.LENGTH_SHORT).show();
-                        Toast.makeText(context, "Mail " + persona1.mail, Toast.LENGTH_SHORT).show();
-                        Toast.makeText(context, "Contrasena " + persona1.contrasena, Toast.LENGTH_SHORT).show();
-                    }
-                });
-
+                goToRegister();
             }
         });
     }
 
-    private void bindUI(){
-        nombre = findViewById(R.id.nombre);
-        apellido = findViewById(R.id.apellido);
-        mail = findViewById(R.id.mail);
-        contrasena = findViewById(R.id.contrasena);
+
+    public void goToRegister(){
+        Intent intent = new Intent(this, RegisterActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
     }
 
 
+
+
+
+
+    /*----------------------------------------------------------------------------------------------------------------*/
+    //NO SE QUE VERGA ES
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
--- app/src/main/java/aplicacion/android/stevemorbo/previasfinal/ServicePersona.java
+++ app/src/main/java/aplicacion/android/stevemorbo/previasfinal/ServicePersona.java
@@ -1,5 +1,13 @@
 package aplicacion.android.stevemorbo.previasfinal;
 
+import android.util.Log;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class ServicePersona {
 
     public static boolean iniciarsesion(String mail, String contrasena){
@@ -8,6 +16,39 @@ public class ServicePersona {
             return true;
         }else {
             return false;
+        }
+    }
+
+    public static boolean registrarse(String nombre, String apellido, String mail, String contrasena){
+
+        if(nombre.equals("") || contrasena.equals("") || contrasena.length()<4){
+            return false;
+        }else {
+
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                DatabaseReference Person = database.getReference("Persona");
+
+                Person.child("nombre").setValue(nombre);
+
+                Person.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        // This method is called once with the initial value and again
+                        // whenever data at this location is updated.
+
+                        /*FIXIT
+                        String value = dataSnapshot.getValue(String.class);
+                        Log.d("nombre", value);
+                        */
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError error) {
+                        // Failed to read value
+                        Log.w("Persona", "Failed to read value.", error.toException());
+                    }
+                });
+            return true;
         }
     }
 
